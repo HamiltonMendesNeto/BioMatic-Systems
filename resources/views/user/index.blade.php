475a2969f4a771e35b42f85ac71d1861841a7cc2
unknown <HamiltonMendesNeto>
2025-03-06 09:53:59 -0300

    atualização pagina de cadastro

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,35 +1,46 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-
+<html lang="pt-br">
 <head>
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <link rel="stylesheet" href="">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="imagens">
 </head>
-
 <body>
 
-        <div class="cabecalho">
+    <div class="campo_verde">
+        <img src="{{ URL::asset('imagens/Captura de tela 2025-02-28 150031.png') }}" class="imagem-capo-verde" alt="Foto Horta">
+    </div>
 
-            <img src="/public/imagens/PXL_20241125_171714142.jpg" alt="">
-
-
-        </div>
-        <div class="formulario">
+    <div class="container">
+        <div class="campoDeCadastro">
             <h1>Bem-Vindo</h1>
-            <h3>Crie sua conta</h3>
-            <div class="campos">
-                <input class="inform" type="text" placeholder="Nome">
-                <input class="inform" type="email" placeholder="E-mail">
-                <input class="inform" type="password" placeholder="Senha">
+            <h2>Crie sua conta</h2>
 
+            <div class="input-group">
+                <img src="{{ URL::asset('imagens/imagem-do-usuario-com-fundo-preto.png') }}" class="input-icon" alt="Usuário">
+                <input class="campos" type="text" placeholder="Nome">
 
             </div>
-            <button onclick="">Cadastrar</button>
-        </div>
 
+            <div class="input-group">
+                <img src="{{ URL::asset('imagens/o-email.png') }}" class="input-icon" alt="E-mail">
+                <input class="campos" type="email" placeholder="E-mail">
+            </div>
+           
+
+            <div class="input-group">
+                <img src="{{ URL::asset('imagens/cadeado.png') }}" class="input-icon" alt="Senha">
+                <input class="campos" type="password" placeholder="Senha">
+            </div>
+            <center> <button id="Cadastrar_button">Cadastrar</button></center>
+           
+        </div>
+    </div>
+    
 
 </body>
-
 </html>
